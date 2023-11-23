046a87a52756e9cc7f510d50464d36fba48a7e84
hellowind <67744274+NineTears@users.noreply.github.com>
2023-11-23 22:13:38 +0800

    支持Turtle-WOW-zhCN_1.17客户端

---
+++ SE/SpecialEvents-Mount-2.0/SpecialEvents-Mount-2.0.lua
@@ -0,0 +1,98 @@
+--[[
+	Name: SpecialEvents-Mount-2.0
+	Revision: $Rev: 15360 $
+	Author: Tekkub Stoutwrithe (tekkub@gmail.com)
+	Website: http://www.wowace.com/
+	Description: Special events for mounting
+	Dependencies: AceLibrary, AceEvent-2.0, Gratuity-2.0
+]]
+
+
+local vmajor, vminor = "SpecialEvents-Mount-2.0", "$Revision: 15360 $"
+
+if not AceLibrary then error(vmajor .. " requires AceLibrary.") end
+if not AceLibrary:HasInstance("AceEvent-2.0") then error(vmajor .. " requires AceEvent-2.0.") end
+if not AceLibrary:IsNewVersion(vmajor, vminor) then return end
+
+local lib = {}
+AceLibrary("AceEvent-2.0"):embed(lib)
+
+-- Activate a new instance of this library
+function activate(self, oldLib, oldDeactivate)
+	if oldLib then
+		self.vars = oldLib.vars
+		oldLib:UnregisterAllEvents()
+	else
+		self.vars = {}
+	end
+	self:RegisterEvent("PLAYER_AURAS_CHANGED")
+	
+	if oldDeactivate then oldDeactivate(oldLib) end
+end
+
+local Mount_List = {
+	"_mount_",						--常规坐骑
+	"spell_nature_swiftness",		--骸骨军马、机械陆行鸟、科多兽、地狱战马、迅猛龙等
+	"_qirajicrystal_",				--其拉共鸣水晶
+	
+	--------特殊坐骑请玩家前往小地图 Automaton-下马-增加坐骑 功能中添加--------
+	"hunter_pet_turtle",   --乌龟坐骑
+	"warstomp",            --斑马坐骑
+	"bullrush",            --幽灵狮鹫
+	"_branch_",            --驯鹿
+	"hunter_pet_hippogryph", --角鹰兽
+	"hunter_pet_tallstrider", --爱情鸟
+	"hunter_pet_bear",     --熊
+	"spell_nature_sentinal", --乌鸦
+	"inv_misc_key_06",     -- 工程坐骑
+	"inv_misc_key_12",     -- 工程坐骑
+	"spell_magic_polymorphchicken", -- 魔法公鸡
+	"ability_racial_bearform",
+	"ability_druid_catform",
+	"ability_druid_travelform",
+    "spell_nature_forceofnature",
+	"ability_druid_aquaticform",
+	"spell_nature_spiritwolf",
+    "ability_hunter_pet_turtle",
+    "inv_pet_speedy",
+    "inv_misc_head_dragon_black", 
+    "ability_bullrush", 
+}
+
+function lib:PLAYER_AURAS_CHANGED()
+	for i = 0, 31, 1 do
+		Mount_Texture = GetPlayerBuffTexture(i)
+		if Mount_Texture then
+			for _, Mount_BuffType in pairs(Mount_List) do
+				if string.find(string.lower(Mount_Texture), Mount_BuffType) then
+					self.vars.mounted, self.vars.buffid = true, i
+					break
+				else
+					self.vars.mounted, self.vars.buffid = false, nil
+				end
+			end
+		end
+	end
+	if Automaton_Dismount ~= nil then
+		for k = 1, table.getn(Automaton_Dismount.db.profile.mounts), 1 do
+			local _, i = UnitHasAura("player", Automaton_Dismount.db.profile.mounts[k])
+			if i then
+				self.vars.mounted, self.vars.buffid = true, i - 1
+				break
+			else
+				self.vars.mounted, self.vars.buffid = false, nil
+			end
+		end
+	end
+end
+
+--返回玩家是否在坐骑上
+function lib:PlayerOnMount()
+	return self.vars.mounted, self.vars.buffid
+end
+
+
+--------------------------------
+--      Load this bitch!      --
+--------------------------------
+AceLibrary:Register(lib, vmajor, vminor, activate)
