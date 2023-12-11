1f267485b75b48d1fa87315227aefd130ee77802
hellowind <67744274+NineTears@users.noreply.github.com>
2023-12-12 00:30:15 +0800

    修正SE库报错(去除凡人添加的全局变量)

--- SE/SpecialEvents-Mount-2.0/SpecialEvents-Mount-2.0.lua
+++ SE/SpecialEvents-Mount-2.0/SpecialEvents-Mount-2.0.lua
@@ -30,18 +30,36 @@ function activate(self, oldLib, oldDeactivate)
 	if oldDeactivate then oldDeactivate(oldLib) end
 end
 
+local Mount_List = {
+	"_mount_",						--常规坐骑
+	"spell_nature_swiftness",		--骸骨军马、机械陆行鸟、科多兽、地狱战马、迅猛龙等
+	"_qirajicrystal_",				--其拉共鸣水晶
+	
+	--------特殊坐骑请玩家前往小地图 Automaton-下马-增加坐骑 功能中添加--------
+}
+
 function lib:PLAYER_AURAS_CHANGED()
 	for i = 0, 31, 1 do
-		local Mount_Texture = GetPlayerBuffTexture(i)
+		Mount_Texture = GetPlayerBuffTexture(i)
 		if Mount_Texture then
-			for _, Mount_BuffType in pairs(Automaton_Dismount.db.profile.mounts) do
-				if string.find(string.lower(Mount_Texture), string.lower(Mount_BuffType)) then
-					self.vars.mounted = true
+			for _, Mount_BuffType in pairs(Mount_List) do
+				if string.find(string.lower(Mount_Texture), Mount_BuffType) then
+					self.vars.mounted, self.vars.buffid = true, i
 					break
 				else
-					self.vars.mounted = false
-					break
+					self.vars.mounted, self.vars.buffid = false, nil
 				end
+			end
+		end
+	end
+	if Automaton_Dismount ~= nil then
+		for k = 1, table.getn(Automaton_Dismount.db.profile.mounts), 1 do
+			local _, i = UnitHasAura("player", Automaton_Dismount.db.profile.mounts[k])
+			if i then
+				self.vars.mounted, self.vars.buffid = true, i - 1
+				break
+			else
+				self.vars.mounted, self.vars.buffid = false, nil
 			end
 		end
 	end
@@ -49,8 +67,9 @@ end
 
 --返回玩家是否在坐骑上
 function lib:PlayerOnMount()
-	return self.vars.mounted
+	return self.vars.mounted, self.vars.buffid
 end
+
 
 --------------------------------
 --      Load this bitch!      --
